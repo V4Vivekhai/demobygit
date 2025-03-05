6802850844d2b5cd87d86205fd6262839d3f15c0
vk <vk99@gmail.com>
2025-03-05 18:42:57 +0530

    dek

---
+++ idx.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 4;
+        int n = 5;
+        int x = a+n;
+        System.out.println(x);
+    }
+}
